03706aed8dca65c43d715b296605ff7c628b3f17
Mougou8 <riquet.mougoundan@gmail.com>
2025-04-01 17:05:44 +0200

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,30 @@
+console.log("Hello JS!");
+let age = 37;
+console.log(age);
+let animal = "chat";
+console.log(animal);
+animal = "cat";
+console.log(animal);
+animal = "bird";
+console.log(animal);
+
+const deux = 2;
+console.log(deux);
+
+const deux = 2;
+console.log(deux);
+let quantite = 3;
+console.log(quantite);
+const prix = 1.15;
+console.log(prix);
+
+
+let numberToGuess = 2;
+numberToGuess += 3;
+numberToGuess -= 2;
+numberToGuess++;
+numberToGuess *= 10;
+numberToGuess /= 2;
+numberToGuess %= 7;
+numberToGuess--;
+console.log("numberToGuess [2 +3 -2 *10 /2 %7 -1] = ", numberToGuess);
